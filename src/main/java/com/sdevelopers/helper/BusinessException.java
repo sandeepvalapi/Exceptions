309984c79a60477d1ddbde9c4b238db0378e9e80
Sandeep Valapi <svalapi@apple.com>
2019-11-22 01:08:33 +0530

    Initial commit to Exceptions

---
+++ src/main/java/com/sdevelopers/helper/BusinessException.java
@@ -0,0 +1,17 @@
+package com.sdevelopers.helper;
+
+/**
+ * <p><b>FILE:</b> BusinessException.java</p>
+ * <p><b>PACKAGE:</b> com.sdevelopers.helper</p>
+ *
+ * <p><b>SUMMARY:</b> INSERT MEANINGFUL CLASS SUMMARY - please
+ * provide as much information about the class; if it relates
+ * directly to a screen or an area of functionality,
+ * then please specify this.</p>
+ *
+ * @author Apple
+ * @since 22/11/19
+ */
+public class BusinessException
+{
+}
